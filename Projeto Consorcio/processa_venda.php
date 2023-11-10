6aebf4bdc267b1bf66cacbbad0e2c2b32186abdb
Heriwelton Rocha <126431760+Heriwelton@users.noreply.github.com>
2023-11-10 12:54:17 -0300

    Update processa_venda.php

--- Projeto Consorcio/processa_venda.php
+++ Projeto Consorcio/processa_venda.php
@@ -76,14 +76,14 @@
 			$mail->isSMTP();                                            //Send using SMTP
 			$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 			$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-			$mail->Username   = 'marcio.consultoria.oficial@gmail.com';                     //SMTP username
-			$mail->Password   = 'jdvmqvpzxkkrolwi';                               //SMTP password
+			$mail->Username   = '/email que irá enviar/';                     //SMTP username
+			$mail->Password   = '/password/';                               //SMTP password
 			$mail->SMTPSecure = 'tls';//PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 			$mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 			//Recipients
-			$mail->setFrom('marcio.consultoria.oficial@gmail.com', 'MARCIO ALCANTARA - Consultor especializado');
-			$mail->addAddress('marcio.consultoria.oficial@gmail.com');     //Add a recipient
+			$mail->setFrom('/email que irá receber/', 'MARCIO ALCANTARA - Consultor especializado');
+			$mail->addAddress('/email que irá receber/');     //Add a recipient
 			//$mail->addAddress('ellen@example.com');               //Name is optional
 			//$mail->addReplyTo('info@example.com', 'Information');
 			//$mail->addCC('cc@example.com');
